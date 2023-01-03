ac452d6450450db9f1e1108c612137543dbca654
she11fish <trolltrolltrolllollollol@outlook.com>
2023-01-03 02:17:02 -0500

    uploading files

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from 'cors'
 import apiRouter from './routes/index.js'
-const app = express();
 
+const app = express();
+const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
@@ -15,4 +16,4 @@ app.use('/api/v1', apiRouter)
 
 app.get('/')
 
-app.listen(5000);
+app.listen(port);
